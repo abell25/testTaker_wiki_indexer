8e2a914362b9dd3295550981a9eb5785a61ac16d
anthony bell <anthony bell>
2015-12-20 16:05:47 -0800

    Make a couple of changes so maven downloads all needed jars.  ALso included a small bit from the wikipedia xml dump for testing purposes.

--- src/main/java/testTaker_wiki_indexer/Main.java
+++ src/main/java/testTaker_wiki_indexer/Main.java
@@ -22,11 +22,16 @@ import org.apache.lucene.store.FSDirectory;
 
 
 public class Main {
+	//TODO: set these to the locations on your computer
+	public static String indexPath = "C:/Users/anthony/PycharmProjects/testTaker_wiki_indexer/indexes/wikipedia";
+	public static String wikipediaXmlDump = "C:/Users/anthony/PycharmProjects/testTaker_wiki_indexer/data/wikipedia_first100k_entries_only.xml";
 
 	public static void main(String[] args) throws Exception {
 		System.out.println("Started!");
 		
 		CreateWikipediaFilesIndex();
+		
+		System.out.println("Testing that the index is working property by querying it..");
 		SearchWikipediaFilesIndex("cell");
 
 		System.out.println("Completed!");
@@ -34,72 +39,15 @@ public class Main {
 	}
 
 	public static void SearchWikipediaFilesIndex(String queryString) throws Exception {
-		String indexPath        = "/media/tony/ssd/TestTakerData/wikipedia_index";
-
 		WikipediaIndexer wikipediaIndexer = new WikipediaIndexer(indexPath);
 		wikipediaIndexer.SearchIndex(queryString);
 	}
 
 	public static void CreateWikipediaFilesIndex() throws Exception {
-		String wikipediaXmlDump = "/media/tony/ssd/TestTakerData/enwiki-20150901-pages-articles.xml";
-		String indexPath        = "/media/tony/ssd/TestTakerData/wikipedia_index";
-		//wikipediaXmlDump        = "/media/tony/ssd/temp/first_100k.xml";
-
 		WikipediaIndexer wikipediaIndexer = new WikipediaIndexer(indexPath);
 		wikipediaIndexer.CreateIndex(wikipediaXmlDump);
 	}
 
-	public static void SearchWikipediaBooksIndex(String queryString) throws Exception {
-		String indexPath = "/media/tony/ssd/TestTakerData/wikipedia_books_index";
-
-		WikipediaIndexer wikipediaIndexer = new WikipediaIndexer(indexPath);
-		wikipediaIndexer.SearchIndex(queryString);
-	}
-
-	public static void CreateWikipediaBooksIndex() throws Exception {
-		String wikipediaXmlDump = "/media/tony/ssd/TestTakerData/enwikibooks-20151102-pages-articles.xml";
-		String indexPath        = "/media/tony/ssd/TestTakerData/wikipedia_books_index";
-
-		//wikipediaXmlDump = "/home/tony/code/python/notebooks/MyLib/TestTaker/enwikibooks-20151102-pages-articles.xml";
-		//indexPath = "/home/tony/code/python/notebooks/MyLib/TestTaker/wikipedia_books_index";
-
-		WikipediaIndexer wikipediaIndexer = new WikipediaIndexer(indexPath);
-		wikipediaIndexer.CreateIndex(wikipediaXmlDump);
-	}
-
-
-	public static void CreateSimpleFilesIndex() {
-		String inDir = "/home/tony/temp/some_files/";
-		String indexDir = "/home/tony/temp/index";
-		String query = "simple";
-
-		inDir = "/media/tony/ssd/temp/some_files";
-		indexDir = "/media/tony/ssd/temp/index";
-
-		try {
-			Files.walkFileTree(Paths.get(inDir), new SimpleFileVisitor<Path>() {
-				@Override
-				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-					//System.out.println("Visiting file: " + file.toString());
-					return FileVisitResult.CONTINUE;
-				}
-			});
-
-		}catch(IOException ex) {
-
-		}
-
-		TestIndexer testIndexer = new TestIndexer();
-		testIndexer.CreateIndex(inDir, indexDir);
-		System.out.println("****************** index created! *****************");
-
-		try {
-			testIndexer.SearchIndex(query, indexDir);
-		} catch (Exception e) {
-			System.out.println("Exception occurred!: " + e.getClass() + "\n    message: " + e.getMessage());
-		}
-
-	}
 }
 
 
